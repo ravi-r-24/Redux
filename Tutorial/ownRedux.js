a2898604e84f049a21da8f3b31d497cc8433b600
k-ravi24 <k.ravi19777@gmail.com>
2024-09-09 22:18:22 +0530

    implemented: store.subscribe

--- Tutorial/ownRedux.js
+++ Tutorial/ownRedux.js
@@ -12,6 +12,12 @@ const reducer = (state, action) => {
         balance: state.balance - action.payload,
       };
     }
+    case "CHECKBALANCE": {
+      return {
+        ...state,
+        balance: state.balance,
+      };
+    }
     default:
       return state;
   }
@@ -31,6 +37,9 @@ let state = initialState;
 // create action
 const depositAction = { type: "DEPOSIT", payload: 12000 };
 const withdrawAction = { type: "WITHDRAW", payload: 5000 };
+const checkBalanceAction = { type: "CHECKBALANCE" };
+
+let listeners = [];
 
 // create own store
 const MyStore = () => {
@@ -40,9 +49,24 @@ const MyStore = () => {
     },
     dispatch: (action) => {
       state = reducer(state, action);
+      listeners.forEach((listener) => listener());
       return state;
     },
-    subscribe: () => {},
+    subscribe: (listener) => {
+      listeners.push(listener);
+      const unSubscribe = () => {
+        const listenerIndex = listeners.findIndex(
+          (existingListener) => existingListener === listener
+        );
+
+        // remove the passed listener from the list
+        if (listenerIndex > -1) {
+          listeners.splice(listenerIndex, 1);
+        }
+      };
+
+      return unSubscribe;
+    },
   };
   return store;
 };
@@ -50,12 +74,20 @@ const MyStore = () => {
 // Call own store
 const store = MyStore();
 
+// subscribe
+const unSubscribe = store.subscribe(() =>
+  console.log("Store updated: ", store.getState())
+);
+
+store.subscribe(() => console.log("Store updated: ", store.getState()));
+// NOTE: Above both are different subscribers
+
 // dispatch
 console.log(store.getState());
 store.dispatch(depositAction);
-console.log(store.getState());
+unSubscribe();
 store.dispatch(withdrawAction);
-console.log(store.getState());
+store.dispatch(checkBalanceAction);
 
 // console
 // console.log(store);
